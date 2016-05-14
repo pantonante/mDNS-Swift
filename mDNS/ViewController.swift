c2b60e5b385eee12ab8536dff3867ac399367551
Pasquale Antonante <p.antonante@gmail.com>
2016-05-14 22:56:08 +0200

    Initial Commit

---
+++ mDNS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mDNS
+//
+//  Created by Pasquale Antonante on 14/05/16.
+//  Copyright © 2016 Pasquale Antonante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
